a7cfc23a6932c54429f046341151fc70567cf478
marko delgadillo <markoneseven@gmail.com>
2017-12-11 19:55:27 -0800

    #8 sort method of the array property solved the challenge.

--- sum-lowest-integers/index.js
+++ sum-lowest-integers/index.js
@@ -1,18 +1,7 @@
-const arr = [12, 2, 344, 11, 117, 8]
+const arr = [12, 2, 344, 11, 117, 8, 3]
 
 function sumTwoSmallestNumbers(numbers) {
-  const newArr = []
-  for (i = 0; i < numbers.length; i++) {
-    if (numbers[i] < numbers[i + 1] || numbers[i] < numbers[i - 1]) {
-      newArr.push(numbers[i])
-    }
-  }
-  console.log(newArr)
-  const low = []
-  for (i = 0; i < newArr.length; i++) {
-    if (newArr[i] < newArr[i + 1] || newArr[i] < newArr[i - 1]) {
-      low.push(newArr[i])
-    }
-  }
-  return low[0] + low[1]
+  numbers.sort(function(a, b) {
+    return a - b
+  })
 }
