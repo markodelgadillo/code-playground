7adc294129a8ebb329df5b1066449ec2820d94cf
marko delgadillo <markoneseven@gmail.com>
2017-12-20 20:37:09 -0800

    #12 Function now passes all tests of integers, decimal numbers, negative numbers and undefined.

--- sum-of-lot/index.js
+++ sum-of-lot/index.js
@@ -1,5 +1,8 @@
 function f(n) {
   let sum = 0
+  if (!parseInt(n) || parseInt(n) < 0 || !Number.isInteger(n)) {
+    return false
+  }
   for (i = 1; i <= n; i++) {
     sum += parseInt(i, 10)
   }
