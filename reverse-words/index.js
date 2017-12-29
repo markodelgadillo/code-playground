5e0d61121bee007e23ebf0bfc5f2096d39e95b19
marko delgadillo <markoneseven@gmail.com>
2017-12-28 16:24:13 -0800

    #22 Commented logical order of instructions to create the function.

---
+++ reverse-words/index.js
@@ -0,0 +1,6 @@
+// for the string, separate each word into it's own string
+// then reverse each word
+// then combine all strings to one strings
+// include spaces in between each word
+
+function reverseWords(string) {}
