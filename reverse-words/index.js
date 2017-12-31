ef5f8c5a65b62878406d83eb42227058d72c47e3
marko delgadillo <markoneseven@gmail.com>
2017-12-30 21:33:07 -0800

    #22 Function to reverse the words required two for loops that would make an array to a string and replace unwanted commas in each loop.

--- reverse-words/index.js
+++ reverse-words/index.js
@@ -3,4 +3,15 @@
 // then combine all strings to one strings
 // include spaces in between each word
 
-function reverseWords(string) {}
+function reverseWords(str) {
+  const arrStr = str.split(' ')
+  const newArr = []
+  const newStr = []
+  for (i = 0; i < arrStr.length; i++) {
+    newArr.push(arrStr[i].split('').reverse())
+  }
+  for (j = 0; j < newArr.length; j++) {
+    newStr.push(newArr[j].toString().replace(/([^A-Za-z.\b])/g, ''))
+  }
+  return newStr.toString().replace(/([^A-Za-z.\b])/g, ' ')
+}
