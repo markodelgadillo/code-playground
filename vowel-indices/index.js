4e06ba330d7e492a8092633d28d80bca3208bfaa
marko delgadillo <markoneseven@gmail.com>
2018-01-02 21:15:17 -0800

    #24 Recommited within the proper issue branch.

--- vowel-indices/index.js
+++ vowel-indices/index.js
@@ -1,14 +1,17 @@
-// first separate the string into an array
-// then find the index of the vowels in the array
-// and return the result
-
 function vowelIndices(word) {
-  let arrWord = word.split('')
-  arrWord.findIndex(function() {
-    for (i = 0; i < arrWord.length; i++) {
-      if (arrWord[i] === 'a' || 'e' || 'i' || 'o' || 'u') {
-        return
-      }
+  const indices = []
+  const arrWord = word.split('')
+  for (i = 0; i < arrWord.length; i++) {
+    if (
+      arrWord[i].toLowerCase() === 'a' ||
+      arrWord[i].toLowerCase() === 'e' ||
+      arrWord[i].toLowerCase() === 'i' ||
+      arrWord[i].toLowerCase() === 'o' ||
+      arrWord[i].toLowerCase() === 'u' ||
+      arrWord[i].toLowerCase() === 'y'
+    ) {
+      indices.push(i + 1)
     }
-  })
+  }
+  return indices
 }
