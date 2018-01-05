71004e19c1c4fb0c4ad9d8a02e66a0dd48e7cd81
marko delgadillo <markoneseven@gmail.com>
2018-01-05 15:48:31 -0800

    #26 Commented out original solution that uses ECMAScript 2017 and included a solution that the previous version of Javascript that satisfied the Code Wars challenge as well as the unit tests I created.

--- expanded-form/index.js
+++ expanded-form/index.js
@@ -1,3 +1,4 @@
+/*
 function expandedForm(num) {
   console.log(num)
   const numArr = num.toString().split('')
@@ -8,6 +9,23 @@ function expandedForm(num) {
       expanded += ' + ' + numArr[i].toString().padEnd(numArr.length - i, '0')
     } else if (numArr[i] > 0) {
       expanded += numArr[i].toString().padEnd(numArr.length - i, '0')
+    }
+  }
+  return expanded
+}
+*/
+
+function expandedForm(num) {
+  const numArr = num.toString().split('')
+  let expanded = ''
+  for (let i = 0; i < numArr.length; i++) {
+    if (numArr[i] > 0 && i > 0) {
+      // originally used .padEnd() but it is not recognized by
+      // Node v6.11.0 and replaced its use with .repeat()
+      expanded +=
+        ' + ' + numArr[i].toString() + '0'.repeat(numArr.length - (i + 1))
+    } else if (numArr[i] > 0) {
+      expanded += numArr[i].toString() + '0'.repeat(numArr.length - (i + 1))
     }
   }
   return expanded
