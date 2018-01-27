6318cc8f55a9aad9be00cfb3f0ff9f156b47b1f0
marko delgadillo <markoneseven@gmail.com>
2018-01-26 17:14:48 -0800

    #35 Corrected what is to be expected from a string to a boolean value and created the function that satisfies all three test conditions.

--- palindrome/index.test.js
+++ palindrome/index.test.js
@@ -3,12 +3,12 @@ const { expect } = require('chai')
 
 describe('Check if it is a palindrome', () => {
   it('when the string IS a palindrome.', () => {
-    expect(palindrome('Anna')).to.equal('true')
+    expect(palindrome('Anna')).to.equal(true)
   })
   it('when the string is a sentence with spaces and punctuation IS a palindrome', () => {
-    expect(palindrome('Ah, Satan sees Natasha!')).to.equal('true')
+    expect(palindrome('Ah, Satan sees Natasha!')).to.equal(true)
   })
   it('when the string is NOT a palindrome.', () => {
-    expect(palindrome('Hello World!')).to.equal('false')
+    expect(palindrome('Hello World!')).to.equal(false)
   })
 })
