01ca002248c26d3a1edf887b32192e7013fdb3ef
marko delgadillo <markoneseven@gmail.com>
2018-02-12 21:48:54 -0800

    #39 Added the provided code from Code Wars.

---
+++ calculate/index.js
@@ -0,0 +1,15 @@
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus(number) {}
+function minus() {}
+function times() {}
+function dividedBy() {}
