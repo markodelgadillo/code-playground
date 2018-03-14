ebd3273ed28ac9ddae5d42a189a91a73ca68fcab
marko delgadillo <markoneseven@gmail.com>
2018-03-13 22:22:53 -0700

    #42 Adding the emtpy function with notes of what the function should do in certain cases.

---
+++ prefill-array/index.js
@@ -0,0 +1,15 @@
+// v can be anything (primitive or otherwise)
+
+// if v is ommited, fill the array with undefined
+
+// if no is 0, return an empty array
+
+// if n is anything other than an integer or
+// integer-formatted string(e.g.'123') that
+// is >=0, throw a TypeError
+
+// When throwing a TypeError, the message should be
+// n is invalid
+// where you replace n for the actual value passed
+
+function prefill(n, v) {}
