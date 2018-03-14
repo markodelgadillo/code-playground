69838db303db90a78f8f18c2d00c2519a0a2227c
marko delgadillo <markoneseven@gmail.com>
2018-03-13 23:55:23 -0700

    #42 Added comments of additional test cases that failed in the code wars attempt.

--- prefill-array/index.test.js
+++ prefill-array/index.test.js
@@ -1,9 +1,16 @@
 const { expect } = require('chai')
 const prefill = require('./index.js')
 
-describe('', () => {
-  it('', () => {
-    expect(prefill(n, v)).to.eql()
+// should throw erro when n is a float
+
+// should throw error when n is infinity
+
+// should throw error when n is a boolean
+
+// 
+describe('Prefill an array', () => {
+  it('will fill the array with each entry being the value of 'v' by 'n' number of times', () => {
+    expect(prefill(3, 7)).to.eql({7,7,7})
   })
   it('', () => {
     expect(prefill(n, v)).to.eql()
