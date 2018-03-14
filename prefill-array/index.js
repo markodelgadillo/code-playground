3698467c07b01f63bc5471680d11b835b9182068
marko delgadillo <markoneseven@gmail.com>
2018-03-13 23:30:34 -0700

    #42 Created the function that satisfies the sample tests in code wars.

--- prefill-array/index.js
+++ prefill-array/index.js
@@ -14,7 +14,12 @@
 
 function prefill(n, v) {
   const arr = []
-  arr.length = n
+  if (n === 0) {
+    return []
+  } else if (isNaN(n) || n < 0) {
+    throw new TypeError(n + ' is invalid')
+  }
+  arr.length = parseInt(n, 10)
   arr.fill(v, 0, n)
   return arr
 }
