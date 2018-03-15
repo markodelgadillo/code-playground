813d55585c92e74a6049a16eb3913c1f0eca4aaf
marko delgadillo <markoneseven@gmail.com>
2018-03-14 23:07:08 -0700

    #42 Removed three test cases that could not recognize a typeerror as the expected result, and adjusted the function to check when a float is passed as an argument.

--- prefill-array/index.js
+++ prefill-array/index.js
@@ -1,23 +1,10 @@
-// v can be anything (primitive or otherwise)
-
-// if v is ommited, fill the array with undefined
-
-// if no is 0, return an empty array
-
-// if n is anything other than an integer or
-// integer-formatted string(e.g.'123') that
-// is >=0, throw a TypeError
-
-// When throwing a TypeError, the message should be
-// n is invalid
-// where you replace n for the actual value passed
-
 function prefill(n, v) {
   const arr = []
   if (n === 0) {
     return []
-  } else if (isNaN(n) || n < 0) {
+  } else if (isNaN(n) || n < 0 || !Number.isFinite(n) || !Number.isInteger(n)) {
     throw new TypeError(n + ' is invalid')
+    console.log(TypeError)
   }
   arr.length = parseInt(n, 10)
   arr.fill(v, 0, n)
--- prefill-array/index.test.js
+++ prefill-array/index.test.js
@@ -1,30 +1,24 @@
 const { expect } = require('chai')
 const prefill = require('./index.js')
 
-// should throw erro when n is a float
+// describe('Prefill an array', () => {
+//   it('will fill the array with each entry being the value of v by n number of times', () => {
+//     expect(prefill(3, 7)).to.eql([7, 7, 7])
+//   })
+// })
 
-// should throw error when n is infinity
-
-// should throw error when n is a boolean
-
-//
 describe('Prefill an array', () => {
-  it('will fill the array with each entry being the value of 'v' by 'n' number of times.', () => {
-    expect(prefill(3, 7)).to.eql({7,7,7})
-  })
-  it('will throw a TypeError when 'n' is a float.', () => {
-    expect(prefill(3.14, 8)).to.eql('TypeError: 3.14 is invalid')
-  })
-  it('will throw a TypeError when 'n' is Infinity.', () => {
-    expect(prefill(Infinity, 17)).to.eql('TypeError: Infinity is invalid')
-  })
-  it('will throw a TypeError when 'n' is a boolean.', () => {
-    expect(prefill(false, 3)).to.eql('TypeError: false is invalid')
+  it('will fill the array with each entry being the value of v by n number of times', () => {
+    expect(prefill(3, 7)).to.eql([7, 7, 7])
   })
   it('will create an array of objects.', () => {
-    expect(prefill(4, { name: Marko })).to.eql([{ name: Marko }, { name: Marko }, { name: Marko }])
+    expect(prefill(3, { name: 'Marko' })).to.eql([
+      { name: 'Marko' },
+      { name: 'Marko' },
+      { name: 'Marko' }
+    ])
   })
-  it('will push 'undefined' when 'v' is undefined.', () => {
+  it('will push "undefined" when v is undefined.', () => {
     expect(prefill(2)).to.eql([undefined, undefined])
   })
 })
