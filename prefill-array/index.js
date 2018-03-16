09ad275c6709009df792cb352f9f14e0345d3cc5
marko delgadillo <markoneseven@gmail.com>
2018-03-15 22:15:00 -0700

    #42 Pass all test but one in code wars at the moment.

--- prefill-array/index.js
+++ prefill-array/index.js
@@ -1,10 +1,9 @@
 function prefill(n, v) {
   const arr = []
-  if (n === 0) {
-    return []
-  } else if (isNaN(n) || n < 0 || !Number.isFinite(n) || !Number.isInteger(n)) {
-    throw new TypeError(n + ' is invalid')
-    console.log(TypeError)
+  if (isNaN(n) || n < 0 || !Number.isFinite(n) || !Number.isInteger(n)) {
+    throw new TypeError(`${n} is invalid`)
+  } else if (n === 0) {
+    return arr
   }
   arr.length = parseInt(n, 10)
   arr.fill(v, 0, n)
