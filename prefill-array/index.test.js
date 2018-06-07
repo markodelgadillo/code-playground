23798f849874ba31fb9a45a7befda43d1ea8aeb0
marko delgadillo <markoneseven@gmail.com>
2018-06-07 16:08:23 -0700

    #42 Updated the test file.

--- prefill-array/index.test.js
+++ prefill-array/index.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai')
+const { expect, bind } = require('chai')
 const prefill = require('./index.js')
 
 // describe('Prefill an array', () => {
@@ -11,6 +11,17 @@ describe('Prefill an array', () => {
   it('will fill the array with each entry being the value of v by n number of times', () => {
     expect(prefill(3, 7)).to.eql([7, 7, 7])
   })
+  // it('will throw a TypeError when n is a float.', () => {
+  //   expect(prefill(3.14, 8)).to.throw(new TypeError('3.14 is invalid'))
+  // })
+  // it('will throw a TypeError when n is Infinity.', () => {
+  //   expect(prefill.bind(prefill(Infinity, 17))).to.throw(
+  //     new TypeError('Infinity is invalid')
+  //   )
+  // })
+  // it('will throw a TypeError when n is a boolean.', () => {
+  //   expect(prefill(false, 3)).to.throw('TypeError: false is invalid')
+  // })
   it('will create an array of objects.', () => {
     expect(prefill(3, { name: 'Marko' })).to.eql([
       { name: 'Marko' },
