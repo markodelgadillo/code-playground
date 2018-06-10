0ef9bbe7ee1319bd88e3e66c24b2d83d2c03ed78
marko delgadillo <markoneseven@gmail.com>
2018-06-09 19:50:20 -0700

    This commit was not meant for issue-44. Revert "#42 Pass all test but one in code wars at the moment."
    This reverts commit 09ad275c6709009df792cb352f9f14e0345d3cc5.

--- prefill-array/index.js
+++ prefill-array/index.js
@@ -1,9 +1,10 @@
 function prefill(n, v) {
   const arr = []
-  if (isNaN(n) || n < 0 || !Number.isFinite(n) || !Number.isInteger(n)) {
-    throw new TypeError(`${n} is invalid`)
-  } else if (n === 0) {
-    return arr
+  if (n === 0) {
+    return []
+  } else if (isNaN(n) || n < 0 || !Number.isFinite(n) || !Number.isInteger(n)) {
+    throw new TypeError(n + ' is invalid')
+    console.log(TypeError)
   }
   arr.length = parseInt(n, 10)
   arr.fill(v, 0, n)
