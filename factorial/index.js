9155a476ee3088fe4163410c19ff473a319af03f
marko delgadillo <markoneseven@gmail.com>
2018-06-11 22:56:32 -0700

    #46 Completed the factorial function that will be used within the final function to complete the exercise.

--- factorial/index.js
+++ factorial/index.js
@@ -5,9 +5,32 @@
     = 120
 */
 
+// not completed
 function going(n) {
+  let u
   while (n > 0) {
-    let fN *= (n-1)
+    if (n === 0) {
+      break
+    } else {
+      console.log(factorial(n))
+      u = 1 / factorial(n)
+    }
   }
-  return n * fN
+}
+
+// completed factorial function
+function factorial(n) {
+  const fN = n
+  let x = n
+  let y = 1
+  while (n > 0) {
+    if (y === fN) {
+      break
+    } else {
+      x *= fN - y
+      n--
+      y++
+    }
+  }
+  return x
 }
