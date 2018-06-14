21ab06f95356df1fd02aa06ba455d11a512e2df6
marko delgadillo <markoneseven@gmail.com>
2018-06-14 00:18:47 -0700

    #46 Created a second while loop function which uses the first factorial function. Still needs fine tuning.

--- factorial/index.js
+++ factorial/index.js
@@ -4,18 +4,32 @@
     = 5*4*3*2*1
     = 120
 */
+/*
+  n! = factorial(n)
+
+  going(n) => (1/n!) * (1! + 2! + 3!... + n!)
+*/
 
 // not completed
 function going(n) {
-  let u
+  let x = n
+  let y = 1
+  let z
   while (n > 0) {
+    z = factorial(y)
+    console.log(n)
     if (n === 0) {
       break
-    } else {
-      console.log(factorial(n))
-      u = 1 / factorial(n)
     }
+    console.log(z)
+    z += z
+    console.log(y)
+    y++
+    console.log(n)
+    n--
   }
+  console.log(z)
+  return 1 / factorial(x) * this.z
 }
 
 // completed factorial function
